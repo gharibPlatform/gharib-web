9e7829e0238d728e9f2acc7f46924700b5717d72
Rayane Boucheraine <rayaneha08@gmail.com>
2024-11-07 20:16:07 +0100

    fix structure

---
+++ src/app/layout.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import ReactQueryProvider from "@/components/ReactQueryProviders";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Welcome Day",
+  description: "Welcome Day project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={`${poppins.className}`}>
+          {children}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+            transition={Bounce}
+          />
+        </body>
+      </html>
+    </ReactQueryProvider>
+  );
+}

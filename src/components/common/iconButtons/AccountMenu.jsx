b55f5919f69870647fa802bd13fe7e8d27b4e11b
KMalek101 <kaoucheaneslive@gmail.com>
2024-11-16 15:51:33 +0100

    Fixed the account menu icon + right elements adjusting

--- src/components/common/iconButtons/AccountMenu.jsx
+++ src/components/common/iconButtons/AccountMenu.jsx
@@ -1,8 +1,7 @@
-import styles from "./ButtonIcon.module.css"
-
+import Image from "next/image";
 function AccountMenu({ toggleUserMenu }) {
     return <>
-        <img onClick={()=>toggleUserMenu(true)} className={styles.img} src="./public/electron.svg" alt="" />
+        <Image src={"/electron.svg"} onClick={()=>toggleUserMenu(true)} class="w-8 h-8 cursor-pointer" width={1} height={1} alt="accountImage" />
     </>    
 }
 

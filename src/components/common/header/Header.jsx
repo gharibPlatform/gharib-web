f4cd3db60caefaf1308a192592c824f5c1ddb0d8
KMalek101 <kaoucheaneslive@gmail.com>
2024-11-22 22:19:45 +0100

    Usermenu onClick handling

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,19 +1,19 @@
 import RightElements from "./RightElements";
 import InputSearch from "../inputs/inputSearch/InputSearch";
 import Gharib from "../gharib/Gharib";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import UserMenu from "../usermenu/UserMenu";
 // import { motion } from 'framer-motion'
 
 function Header() {
     const [toggleUserMenuBool, setToggleUserMenuBool] = useState(false);
-    const nodeRef = useRef(null);
     
     const toggleUserMenu = (prop) => {
         if (toggleUserMenuBool) {
             setToggleUserMenuBool(!toggleUserMenuBool);
             return;
         }
+
         setToggleUserMenuBool(prop);
         console.log(prop)
     }
@@ -21,11 +21,12 @@ function Header() {
     return <>
     
         <div class="flex items-center justify-between bg-[var(--main-color)] border-b border-[var(--g-color)] pt-2 pb-3">
-            {/* gharib icon */}
             <Gharib />
             <InputSearch />
             <RightElements toggleUserMenu={toggleUserMenu} />
         </div>
+
+        {toggleUserMenuBool ? <UserMenu toggleUserMenu={toggleUserMenu} /> : <div />}
 
     </>
 }

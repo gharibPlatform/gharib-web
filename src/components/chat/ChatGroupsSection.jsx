3e137c9cc25cb601795a90d2d847d9636ccc507e
KMalek101 <kaoucheaneslive@gmail.com>
2024-11-30 19:52:01 +0100

    Applying same logic for the groups

--- src/components/chat/ChatGroupsSection.jsx
+++ src/components/chat/ChatGroupsSection.jsx
@@ -1,13 +1,29 @@
 import ChatGroupCard from "./ChatGroupCard"
-
+import { useState } from "react";
 export default function ChatGroupsSection({ changeNameHeader }) {
+
+    const ARRAY_LENGTH = 5;
+    const BACKGROUND_COLOR = "#212121"
+    const BACKGROUND_COLOR_NEW = "#323232"
+
+    const groupsDataArray = ["Muslims", "Brothers", "2CP5", "2CP1", "2CP3"];
+    const [backgroundColorArray, setBackgroundColorArray] = useState(Array(ARRAY_LENGTH).fill(BACKGROUND_COLOR))
+
+    const handleClick = (group, i) => {
+
+        setBackgroundColorArray(prevBackgroundColor => 
+            prevBackgroundColor.map((element, index) => 
+                (index === i ? `${BACKGROUND_COLOR_NEW}` : `${BACKGROUND_COLOR}`)
+            )
+        )
+
+        changeNameHeader(group);
+    }
+
     return(
         <div >
-            <ChatGroupCard changeNameHeader={changeNameHeader} Name={"Muslims"} />
-            <ChatGroupCard changeNameHeader={changeNameHeader} Name={"Brothers"} />
-            <ChatGroupCard changeNameHeader={changeNameHeader} Name={"2CP5"} />
-            <ChatGroupCard changeNameHeader={changeNameHeader} Name={"2CP1"} />
-            <ChatGroupCard changeNameHeader={changeNameHeader} Name={"2CP3"} />
+            {groupsDataArray.map( (group, index) => (
+                <ChatGroupCard backgroundColor={backgroundColorArray[index]} key={index} index={index} handleClick={handleClick} Name={group} />))}
         </div>
     )
 }

7a9c1a0fa7694f853cdfa0f66bc367519eec756b
KMalek101 <kaoucheaneslive@gmail.com>
2024-11-30 22:32:05 +0100

    font final config

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Home from "@/pages/Home";
 
 export default function page() {
   return (
-    <Home />
-    // <ChatPage />
+    // <Home />
+    <ChatPage />
   );
 }
--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,9 +1,17 @@
+'use client'
 import Header from "@/components/common/header/Header";
 import SideBar from "@/components/common/sidebar/Sidebar";
 import ChatLeftBar from "@/components/chat/ChatLeftBar";
 import "../../app/globals.css"
 import ChatContent from "@/components/chat/ChatContent";
 import { useState } from "react";
+import { Cairo } from "next/font/google";
+
+const cairo = Cairo({
+    subsets: ['latin'], 
+    weight: ['400', '700'], 
+    variable: '--font-cairo', 
+  });
 
 export default function ChatPage() {
 
@@ -13,12 +21,14 @@ export default function ChatPage() {
     }
 
     return<>
-        <Header />
-        
-        <div class="flex h-[var(--height)]">
-            <SideBar />
-            <ChatContent nameHeader={nameHeader} />
-            <ChatLeftBar changeNameHeader={changeNameHeader} />
-        </div>
+        <main className={cairo.variable} >
+            <Header />
+            
+            <div class="flex h-[var(--height)]">
+                <SideBar />
+                <ChatContent nameHeader={nameHeader} />
+                <ChatLeftBar changeNameHeader={changeNameHeader} />
+            </div>
+        </main>
     </>
 }

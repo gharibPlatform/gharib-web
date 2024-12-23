f2d3ed28c288306e2d1898d306131bf307c9fc91
KMalek101 <kaoucheaneslive@gmail.com>
2024-12-23 17:45:31 +0100

    deletion of prisma

--- prisma.js
+++
@@ -1,7 +0,0 @@
-// prisma.js
-import { PrismaClient } from "@prisma/client";
-
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
-export { prisma };

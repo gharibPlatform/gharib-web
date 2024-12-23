7d0d71183e84104c3f3e6a1a42646baabfd73ef2
KMalek101 <kaoucheaneslive@gmail.com>
2024-12-23 16:13:17 +0100

    Prisma config

---
+++ prisma.js
@@ -0,0 +1,7 @@
+// prisma.js
+import { PrismaClient } from "@prisma/client";
+
+const prisma = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export { prisma };

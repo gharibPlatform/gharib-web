34400b35d47921e70055eae7eb24527c35e50f5f
Malek <kaoucheaneslive@gmail.com>
2025-02-13 19:43:21 +0100

    added boundries for the routing in chapters

--- src/app/quran/chapters/[id]/page.jsx
+++ src/app/quran/chapters/[id]/page.jsx
@@ -3,14 +3,19 @@ import QuranContent from "@/components/common/quran/QuranContent";
 import { useParams } from "next/navigation";
 import { getChapter } from "@/utils/quran/quran";
 import useQuranHeaderChapter from "@/stores/chapterQuranHeaderStore";
+import { useEffect } from "react";
 
 const Page = () => {
     const { id } = useParams();
     const setQuranHeaderChapter = useQuranHeaderChapter((state) => state.setQuranHeaderChapter);
-    getChapter(id)
-    .then((resp) => {
-        setQuranHeaderChapter(resp);
-    })
+    useEffect(()=>{
+        if (id >= 1 && id <= 114) {
+            getChapter(id)
+            .then((resp) => {
+                setQuranHeaderChapter(resp);
+            })            
+        }
+    }, [id])
     
     return (
         <div>

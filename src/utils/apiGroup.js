03bd24c2ada8456998a4e69084948b7ff00f0ff1
Malek <kaoucheaneslive@gmail.com>
2025-02-24 00:02:27 +0100

    added the url for the api functions

--- src/utils/apiGroup.js
+++ src/utils/apiGroup.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://your-api-url.com";
+const API_BASE_URL = 'http://localhost:8000'; 
 
 // Group APIs
 export const getGroups = async () => {
--- src/utils/apiKhatma.js
+++ src/utils/apiKhatma.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+const API_BASE_URL = 'http://localhost:8000'; 
+
 export const createKhatma = async (data) => {
     return await axios.post(`${API_BASE_URL}/khatma/create-khatma/`, data);
   };

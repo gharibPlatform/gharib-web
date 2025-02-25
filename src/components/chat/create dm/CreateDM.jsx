bdae9101a7bba8ff839f5444d6b918334897742f
Malek <kaoucheaneslive@gmail.com>
2025-02-25 07:34:23 +0100

    button styling

--- src/components/chat/create dm/CreateDM.jsx
+++ src/components/chat/create dm/CreateDM.jsx
@@ -14,16 +14,17 @@ export default function CreateDM() {
     return (
         <div
             style={{ width: "620px", height: "400px" }}
-            className="bg-[var(--main-color)] pt-4 px-4 rounded-sm border border-[var(--g-color)] flex flex-col"
+            className="pb-4 bg-[var(--main-color)] pt-4 px-4 rounded-sm border border-[var(--g-color)] flex flex-col"
         >
             <h2 className="text-[var(--w-color)] text-2xl py-4">Select Brothers</h2>
             
             <div className="bg-[var(--dark-color)] text-[var(--w-color)] rounded-[5px] border border-[var(--g-color)] py-2 px-4 text-lg flex flex-wrap items-center gap-2">
+
                 {selectedUsers.map((user) => (
                     <span 
                         key={user}
                         onClick={() => toggleUser(user)} 
-                        className= "cursor-pointer bg-[var(--main-color-hover)] px-2 py-2 rounded-md text-sm flex items-center gap-1"
+                        className= "cursor-pointer bg-[var(--main-color-hover)] px-2 py-2 rounded-md text-sm flex items-center gap-2"
                     >
                         <h2>{user}</h2>
                         <h2>✕</h2>
@@ -43,7 +44,14 @@ export default function CreateDM() {
 
             <CreateDMListingBrothers selectedUsers={selectedUsers} toggleUser={toggleUser} />
 
-            <button>Create DM</button>
+            <div className="pb-4"></div>
+
+            <button
+             className=" py-2 px-4 text-[var(--w-color)] bg-[var(--b-color)] rounded-[4px]"
+             >
+                Create DM
+            </button>
+
         </div>
     );
 }

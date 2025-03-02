39be930e51979b283153eaa38b1738aa863a178f
Malek <kaoucheaneslive@gmail.com>
2025-03-02 03:40:16 +0100

    Edit group component

--- src/components/chat/group sidebar/GroupSidebar.jsx
+++ src/components/chat/group sidebar/GroupSidebar.jsx
@@ -2,12 +2,28 @@ import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import OpenMenu from "@/components/common/open menu/OpenMenu";
 import Tooltip from "@/components/common/tooltip/Tooltip";
+import EditGroupSettings from "../groups/EditGroupSettings";
 
 export default function GroupSideBar() {
   const brothersDataArray = ["Malek", "Moh", "Zohir", "Walid", "Moussa"];
   const adminName = "Walid"; 
   const [activeUser, setActiveUser] = useState(null); 
   const menuRef = useRef(null);
+
+  const [showCreateDMConfirmation, setShowCreateDMConfirmation] = useState(false);
+  const createDMRef = useRef(null);
+
+  // Close the modal when clicking outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (createDMRef.current && !createDMRef.current.contains(event.target)) {
+        setShowCreateDMConfirmation(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
 
   // Close menu when clicking outside
   useEffect(() => {
@@ -23,13 +39,25 @@ export default function GroupSideBar() {
   return (
     <div className="flex justify-between flex-col p-6 w-[360px] border-l border-[var(--g-color)] bg-[var(--main-color)] h-[var(--height)]">
       <div className="flex-col flex-nowrap w-full h-min">
-        <div className="w-full py-2 text-lg font-semibold mb-5 text-[var(--w-color)] flex  gap-12 items-center">
+        <div
+            className="w-full py-2 text-lg font-semibold mb-5 text-[var(--w-color)] flex  gap-12 items-center"
+            onClick={() => setShowCreateDMConfirmation(true)}>
+
             Edit group settings
             <button className="flex gap-2 items-center justify-center rounded-sm py-2 bg-[var(--darker-color)] px-4 text-[var(--w-color)] hover:bg-[var(--dark-color)]">
                 Edit
                 <svg className="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
             </button>
         </div>
+
+        {showCreateDMConfirmation && (
+        <div className="fixed inset-0 bg-black bg-opacity-10 flex justify-center items-center z-50">
+          <div className="absolute inset-0 pointer-events-none"></div>
+          <div ref={createDMRef} className=" p-5">
+            <EditGroupSettings selectedUsers={brothersDataArray} />
+          </div>
+        </div>
+      )}
 
         <h2 className="text-lg font-semibold text-[var(--w-color)] mb-2">Group Members</h2>
         <div className="mb-4" ref={menuRef}>

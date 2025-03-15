5dc4639434b0b8a07f59fbe211b0979bfe98b457
Malek <kaoucheaneslive@gmail.com>
2025-03-15 02:33:27 +0100

    Added the quranHeader to the ceateKhatma

--- src/components/chat/khatmas/CreateKhatma.jsx
+++ src/components/chat/khatmas/CreateKhatma.jsx
@@ -1,4 +1,131 @@
 import { useState, useRef, useEffect } from "react";
+import QuranHeaderSection from "@/components/common/quran/quran header/QuranHeaderSection";
+import { listChapters } from "@/utils/quran/quran";
+import ChapterDropdown from "@/components/common/quran/quran header/dropdown/ChapterDropdown";
+import VerseDropdown from "@/components/common/quran/quran header/dropdown/VerseDropdown";
+import PageDropdown from "@/components/common/quran/quran header/dropdown/PageDropdown";
+
+const QuranHeader = () => {
+    const [quranHeaderData, setQuranHeaderData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedChapter, setSelectedChapter] = useState();
+    const [selectedVerse, setSelectedVerse] = useState();
+    const [selectedPage, setSelectedPage] = useState();
+
+    const chapterButtonRef = useRef(null);
+    const verseButtonRef = useRef(null);
+    const pageButtonRef = useRef(null);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+            listChapters().then((resp) => {
+                setQuranHeaderData(resp);
+            });
+        }, []);
+    
+        const [sections, setSections] = useState({
+            chapter: { rotation: 90, isOpen: false },
+            verse: { rotation: 90, isOpen: false },
+            page: { rotation: 90, isOpen: false },
+        });
+    
+        const toggleSection = (section, e) => {
+            e.stopPropagation();
+            setSections((prev) => ({
+                ...prev,
+                [section]: {
+                    rotation: prev[section].isOpen ? 90 : 270,
+                    isOpen: !prev[section].isOpen,
+                },
+            }));
+        };
+
+    const handleClickOutside = (event) => {
+        const isInsideChapterButton = chapterButtonRef.current && chapterButtonRef.current.contains(event.target);
+        const isInsideVerseButton = verseButtonRef.current && verseButtonRef.current.contains(event.target);
+        const isInsidePageButton = pageButtonRef.current && pageButtonRef.current.contains(event.target);
+        const isInsideDropdown = dropdownRef.current && dropdownRef.current.contains(event.target);
+
+        if (!isInsideDropdown && !isInsideChapterButton && !isInsideVerseButton && !isInsidePageButton) {
+            setSections({
+                chapter: { rotation: 90, isOpen: false },
+                verse: { rotation: 90, isOpen: false },
+                page: { rotation: 90, isOpen: false },
+            });
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const sectionsData = [
+        { name: "Chapter", value: selectedChapter ? selectedChapter.name_simple : "Select Chapter", ref: chapterButtonRef },
+        { name: "Verse", value: selectedVerse ? `Verse ${selectedVerse}` : "Select Verse", ref: verseButtonRef },
+        { name: "Page", value: selectedPage ? `Page ${selectedPage}` : "Select Page", ref: pageButtonRef },
+    ];
+
+    const filteredChapters = quranHeaderData.filter((chapter) =>
+        chapter.name_simple.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    return(
+        <div className="w-[var(--header-width)] h-14 bg-[var(--dark-color)] rounded-sm flex justify-between px-6 ml-auto mr-auto scroll-mt-16">
+            {sectionsData.map((section, index) => (
+                <QuranHeaderSection
+                    key={index}
+                    section={section}
+                    sections={sections}
+                    toggleSection={toggleSection}
+                    buttonRef={section.ref} 
+                >
+                    {sections[section.name.toLowerCase()].isOpen && section.name === "Chapter" && (
+                        <ChapterDropdown
+                            dropdownRef={dropdownRef}
+                            searchQuery={searchQuery}
+                            setSearchQuery={setSearchQuery}
+                            filteredChapters={filteredChapters}
+                            onSelectChapter={(chapter, e) => {
+                                // setQuranHeaderChapter(chapter)
+                                // setGoToPath(true);
+                                toggleSection(section.name.toLowerCase(), e);
+                            }}
+                        />
+                    )}
+
+                    {sections[section.name.toLowerCase()].isOpen && section.name === "Page" && (
+                        <PageDropdown
+                            dropdownRef={dropdownRef}
+                            searchQuery={searchQuery}
+                            setSearchQuery={setSearchQuery}
+                            onSelectPage={(page, e)=>{
+                                // setSelectedPage(page)
+                                // setGoToPathPages(true);
+                                toggleSection(section.name.toLowerCase(), e)
+                            }}
+                        />
+                    )}
+
+                    {sections[section.name.toLowerCase()].isOpen && section.name === "Verse" && (
+                        <VerseDropdown
+                            dropdownRef={dropdownRef}
+                            searchQuery={searchQuery}
+                            setSearchQuery={setSearchQuery}
+                            selectedChapter={selectedChapter}
+                            onSelectVerse={(verse, e)=>{
+                                toggleSection(section.name.toLowerCase(), e)
+                                // setSelected(selectedChapter, null, verse)
+                            }}
+                        />
+                    )}
+                </QuranHeaderSection>
+            ))}
+        </div>
+    )
+}
 
 export default function CreateKhatma() {
     const [isLimited, setIsLimited] = useState(null);
@@ -97,6 +224,8 @@ export default function CreateKhatma() {
                             </form>
                         </div>
 
+                        {/* quranHeader section */}
+                        <QuranHeader />
                         <div className="pb-4"></div>
 
                         {/* Submit Button */}

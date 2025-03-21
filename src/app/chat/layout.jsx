ce8e59ca88c043d83ebf67d33fe5474b49286261
Malek <kaoucheaneslive@gmail.com>
2025-03-21 16:15:48 +0100

    Context usage in app

--- src/app/chat/layout.jsx
+++ src/app/chat/layout.jsx
@@ -4,25 +4,28 @@ import GroupSideBar from "@/components/chat/group sidebar/GroupSidebar";
 import Header from "@/components/common/header/Header";
 import SideBar from "@/components/common/sidebar/Sidebar";
 import useGroupSidebarStore from "@/stores/groupSidebarStore";
+import { ChatActionsProvider } from "@/context/ChatActionContext";
 
 const Layout = ({ children }) => {
   const { isGroupSidebarOpen }  = useGroupSidebarStore();
 
   return (
-    <div className="w-screen overflow-hidden h-screen flex flex-col">
-      <Header />
-      <div className="flex">
-        <SideBar />
-        <div className="ml-72 pt-14 flex w-full">
-          <div className="h-screen flex-grow">
-            {children}
-          </div>
-          <div>
-            {isGroupSidebarOpen ? <GroupSideBar /> : <ChatRightBar />}
+    <ChatActionsProvider>
+      <div className="w-screen overflow-hidden h-screen flex flex-col">
+        <Header />
+        <div className="flex">
+          <SideBar />
+          <div className="ml-72 pt-14 flex w-full">
+            <div className="h-screen flex-grow">
+              {children}
+            </div>
+            <div>
+              {isGroupSidebarOpen ? <GroupSideBar /> : <ChatRightBar />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ChatActionsProvider>
   );
 };
 

5499630642c3e8953c86c7999a54f6c56a0ce59b
Malek <kaoucheaneslive@gmail.com>
2025-03-21 17:40:20 +0100

    KhatmaProvider usage

--- src/app/khatmas/layout.jsx
+++ src/app/khatmas/layout.jsx
@@ -2,21 +2,24 @@
 import ChatRightBar from "@/components/chat/ChatRightBar";
 import Header from "@/components/common/header/Header";
 import SideBar from "@/components/common/sidebar/Sidebar";
+import { KhatmaProvider } from "@/context/KhatmaContext";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-screen overflow-hidden h-screen flex flex-col">
-      <Header />
-      <div className="flex">
-        <SideBar />
-        <div className="ml-72 pt-14 flex w-full">
-          <div className="h-screen flex-grow">{children}</div>
-          <div>
-            <ChatRightBar />
+    <KhatmaProvider>
+      <div className="w-screen overflow-hidden h-screen flex flex-col">
+        <Header />
+        <div className="flex">
+          <SideBar />
+          <div className="ml-72 pt-14 flex w-full">
+            <div className="h-screen flex-grow">{children}</div>
+            <div>
+              <ChatRightBar />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </KhatmaProvider>
   );
 };
 

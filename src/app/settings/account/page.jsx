7cb389b556bf75071527512da0f793d693219b80
Malek <kaoucheaneslive@gmail.com>
2025-06-08 18:04:29 +0100

    settings routes pages

---
+++ src/app/settings/account/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Header from "@/components/common/header/Header"
+import SettingsSideBar from "@/components/settings/SettingsSideBar"
+
+export default function page() {
+  return(
+  <div className="bg-[#fff] w-screen">
+    <Header />
+    <SettingsSideBar />
+    {/* <AccountSettings /> */}
+  </div>
+  )
+}

d9b88c6a67346524c1217de10aa559d22857a0d8
Malek <kaoucheaneslive@gmail.com>
2025-06-11 13:05:26 +0100

    Quran settings set up

--- src/app/settings/quran/page.jsx
+++ src/app/settings/quran/page.jsx
@@ -1,13 +1,11 @@
 "use client"
 
-import Header from "@/components/common/header/Header"
-import SettingsSideBar from "@/components/settings/SettingsSideBar"
+import QuranSettings from "@/components/settings/QuranSettings"
 
 export default function page() {
   return(
-  <div className="bg-[#fff] w-screen">
-    <Header />
-    <SettingsSideBar />
+  <div className="w-screen">
+    <QuranSettings />    
   </div>
   )
 }
---
+++ src/components/settings/QuranSettings.jsx
@@ -0,0 +1,7 @@
+export default function QuranSettings() {
+    return(
+        <div>
+            
+        </div>
+    )
+}

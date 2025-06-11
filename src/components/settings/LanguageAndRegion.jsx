06dc922e14a14e54c7bdf468d6391df3f31b2bba
Malek <kaoucheaneslive@gmail.com>
2025-06-11 13:39:50 +0100

    onBlur effect

--- src/components/settings/LanguageAndRegion.jsx
+++ src/components/settings/LanguageAndRegion.jsx
@@ -59,6 +59,7 @@ export default function LanguageAndRegion() {
                         <select
                             value={language}
                             onChange={handleLanguageChange}
+                            onBlur={() => setRotate(90)}
                             className="w-min bg-[var(--main-color)] border border-[var(--g-color)] rounded px-4 py-2 text-white focus:outline-none focus:border-[var(--main-color-hover)] appearance-none pr-8"
                         >
                             {availableLanguages.map((lang) => (

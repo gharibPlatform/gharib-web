cbf880773e06d5e2a5ee1630cd5eb046bd43d501
Malek <kaoucheaneslive@gmail.com>
2025-06-11 14:16:24 +0100

    no overflow and scrollbar hidden in all the site

--- src/app/settings/layout.jsx
+++ src/app/settings/layout.jsx
@@ -4,12 +4,12 @@ import SettingsSideBar from "@/components/settings/SettingsSideBar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-screen overflow-hidden h-screen flex flex-col">
+    <div className="w-screen h-screen flex flex-col">
         <Header />
         <div className="flex">
         <SettingsSideBar />
-        <div className="ml-72 pt-14 flex w-full">
-            <div className="h-screen flex-grow">
+        <div className="ml-72 pt-14 flex w-full no-scrollbar ">
+            <div className=" flex-grow no-scrollbar ">
                 {children}
             </div>
         </div>
--- src/app/settings/quran/page.jsx
+++ src/app/settings/quran/page.jsx
@@ -4,7 +4,7 @@ import QuranSettings from "@/components/settings/QuranSettings"
 
 export default function page() {
   return(
-  <div className="w-screen">
+  <div className="w-screen no-scrollbar ">
     <QuranSettings />    
   </div>
   )

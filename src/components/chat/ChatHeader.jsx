43da794fcbe1bff190e98278e668b48d89731bfb
KMalek101 <kaoucheaneslive@gmail.com>
2025-07-16 14:35:09 +0100

    OnClick show sidebar with a info svg

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -3,16 +3,16 @@
 import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
-import useGroupSidebarStore from "../../stores/groupSidebarStore"; 
+import useGroupSidebarStore from "../../stores/groupSidebarStore";
 import OpenMenu from "../common/open menu/OpenMenu";
 
 export default function ChatHeader({ Name }) {
   const [isClicked, setIsClicked] = useState(false);
   const menuRef = useRef(null);
   const menuButtonRef = useRef(null);
-  const pathname = usePathname(); 
+  const pathname = usePathname();
   const { isGroupSidebarOpen, setGroupSidebar } = useGroupSidebarStore();
-
+  //#585858 svg color
   // Check if the route matches "/chat/brothers/[name]" or "/chat/groups/[name]"
   const isBrotherRoute = pathname.startsWith("/chat/brothers/");
   const isGroupRoute = pathname.startsWith("/chat/groups/");
@@ -46,7 +46,7 @@ export default function ChatHeader({ Name }) {
           height={1}
           alt="accountImage"
         />
-        
+
         {/* Clicking the title toggles Zustand state for groups */}
         <h2
           className="text-[var(--w-color)] text-xl cursor-pointer"
@@ -58,7 +58,30 @@ export default function ChatHeader({ Name }) {
         >
           {Name}
         </h2>
-
+        <svg
+          className={`w-8 h-8 ml-auto cursor-pointer`}
+          onClick={() => setGroupSidebar(!isGroupSidebarOpen)}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M12 7.01001V7.00002M12 17L12 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              stroke={isGroupSidebarOpen ? "#fff" : "#585858"}
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>{" "}
+          </g>
+        </svg>
         {/* Show the menu button only for "brother/[name]" */}
         {isBrotherRoute && (
           <div

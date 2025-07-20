657f21ae448023eb7db0819713b32a438d5134f9
KMalek101 <kaoucheaneslive@gmail.com>
2025-07-20 03:15:47 +0100

    Made the settings sidebar elements save color on click

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import ChatButton from "../buttons/ChatButton.jsx";
 import ExploreButton from "../buttons/ExploreButton";
 import HomeButton from "../buttons/HomeButton";
 import HumburgerMenu from "../buttons/HumburgerMenu.jsx";
-
 export default function SideBar() {
   return (
     <div className="bg-[var(--dark-color)] p-2 flex flex-col gap-6 items-center h-screen border-r border-[var(--g-color)]">
--- src/components/settings/SettingsSideBar.jsx
+++ src/components/settings/SettingsSideBar.jsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function SettingsSideBar() {
     return (
         <div className="h-screen border-r border-[var(--g-color)] bg-[var(--main-color)] inline-block">
-            <div className="inline-block flex-col pt-4">
+            <div className="inline-block flex-col pt-4 px-2">
                 <SettingsItem title="Account" path="/settings/account" />
                 <SettingsItem title="Profile" path="/settings/profile" />
                 <SettingsItem title="Appearance" path="/settings/appearance" />
@@ -19,6 +20,9 @@ export default function SettingsSideBar() {
 
 function SettingsItem({ title, path }) {
     const router = useRouter();
+    const currentPath = usePathname();
+
+    const isActive = currentPath === path;
 
     const handleClick = () => {
         router.push(path);
@@ -27,7 +31,9 @@ function SettingsItem({ title, path }) {
     return (
         <div
             onClick={handleClick}
-            className="hover:bg-[var(--secondary-color)] pt-3 pb-3 flex flex-inline items-center gap-4 cursor-pointer pl-9 pr-28"
+            className={`hover:bg-[var(--main-color-hover)] rounded-sm pt-3 pb-3 flex flex-inline items-center gap-4 cursor-pointer pl-9 pr-28 duration-100 transition-all ease-in ${
+                isActive ? "bg-[var(--main-color-hover)]" : ""
+            }`}
         >
             <p className="text-r text-white">{title}</p>
         </div>

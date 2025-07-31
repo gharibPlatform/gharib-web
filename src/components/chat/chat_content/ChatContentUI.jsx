dbbc8d3871518e67560463a7f7b1d2366de12338
KMalek101 <kaoucheaneslive@gmail.com>
2025-07-31 16:30:25 +0100

    deleted the chatUI library

--- src/components/chat/chat_content/ChatContentUI.jsx
+++ src/components/chat/chat_content/ChatContentUI.jsx
@@ -1,43 +1,9 @@
 import React from "react";
-import ChatUI, { Bubble, useMessages } from "@chatui/core";
-import "@chatui/core/dist/index.css";
+import "../../../app/globals.css";
 
 export default function ChatContentUI() {
-  const { messages, appendMsg } = useMessages([]);
-
-  function handleSend(type, val) {
-    if (type === "text" && val.trim()) {
-      appendMsg({
-        type: "text",
-        content: { text: val },
-        position: "right",
-      });
-
-      setTimeout(() => {
-        appendMsg({
-          type: "text",
-          content: { text: "Hi, I'm ChatUI" },
-        });
-      }, 1000);
-    }
-  }
-
-  function renderMessageContent(msg) {
-    const { type, content } = msg;
-
-    switch (type) {
-      case "text":
-        return <Bubble content={content.text} />;
-      default:
-        return null;
-    }
-  }
 
   return (
-    <ChatUI
-      messages={messages}
-      renderMessageContent={renderMessageContent}
-      onSend={handleSend}
-    />
+    <div></div>
   );
 }

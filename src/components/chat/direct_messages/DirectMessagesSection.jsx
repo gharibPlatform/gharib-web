87472f7d487d82c6982763b6fc1e051113b104f6
KMalek101 <kaoucheaneslive@gmail.com>
2025-08-13 15:35:10 +0100

    onClick go to group

--- src/components/chat/direct_messages/DirectMessagesSection.jsx
+++ src/components/chat/direct_messages/DirectMessagesSection.jsx
@@ -1,13 +1,23 @@
 import DirectMessagesCard from "./DirectMessagesCard";
 import useGroupStore from "../../../stores/groupStore";
+import { useRouter } from "next/navigation";
 
 export default function DirectMessagesSection() {
   const { groups } = useGroupStore();
+  const router = useRouter();
+
+  const handleClick = (group) => {
+    router.push(`/chat/groups/${group.id}`);
+  };
 
   return (
     <div className="flex flex-col pt-6">
       {groups?.results.map((group) => (
-        <div key={group.id} className="flex justify-center items-center">
+        <div
+          key={group.id}
+          onClick={() => handleClick(group)}
+          className="flex justify-center items-center"
+        >
           <DirectMessagesCard
             name={group.name}
             icon={"/electron.svg"}

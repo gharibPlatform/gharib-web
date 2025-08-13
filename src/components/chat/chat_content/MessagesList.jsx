893ea0d2f270e9e13d64a2baed0d03b2fa08065b
fratsaislam <i_fratsa@estin.dz>
2025-08-13 15:49:34 +0100

    Added 'framer-motion' in messages

--- src/components/chat/chat_content/MessagesList.jsx
+++ src/components/chat/chat_content/MessagesList.jsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import Bubble from "./Bubble";
+import { motion, AnimatePresence } from "framer-motion";
 
 const MessagesList = ({ messages, currentUserId }) => {
   const formatTime = (isoString) => {
     const date = new Date(isoString.replace(/(\.\d{3})\d+Z$/, "$1Z"));
-    
     return date.toLocaleTimeString("en-US", {
       hour: "numeric",
       minute: "2-digit",
@@ -15,15 +16,22 @@ const MessagesList = ({ messages, currentUserId }) => {
   return (
     <>
       {messages.map((message) => (
-        <Bubble
+        <motion.div
           key={message.id}
-          text={message.message}
-          username={message.sender.username}
-          isSent={message.sender.id === currentUserId}
-          timestamp={formatTime(message.updated_at)}
-          showAvatar={message.sender.id !== currentUserId}
-          avatarUrl={message.sender.profile_pic || ""}
-        />
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <Bubble
+            text={message.message}
+            username={message.sender.username}
+            isSent={message.sender.id === currentUserId}
+            timestamp={formatTime(message.updated_at)}
+            showAvatar={message.sender.id !== currentUserId}
+            avatarUrl={message.sender.profile_pic || ""}
+          />
+        </motion.div>
       ))}
     </>
   );

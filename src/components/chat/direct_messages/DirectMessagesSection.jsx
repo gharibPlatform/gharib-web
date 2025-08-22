df5ef9a1230d10de0dd027a3e15942345963cbdc
fratsaislam <i_fratsa@estin.dz>
2025-08-22 18:37:58 +0100

    add onClick when clicking on groups in dm

--- src/components/chat/direct_messages/DirectMessagesSection.jsx
+++ src/components/chat/direct_messages/DirectMessagesSection.jsx
@@ -1,13 +1,22 @@
 import DirectMessagesCard from "./DirectMessagesCard";
 import useGroupStore from "../../../stores/groupStore";
-
+import { useRouter } from "next/navigation";
 export default function DirectMessagesSection() {
   const { groups } = useGroupStore();
+  const router = useRouter()
+
+  const handleGroupClick = (groupId) => {
+    router.push(`/chat/groups/${groupId}`);
+  };
 
   return (
     <div className="flex flex-col pt-6">
       {groups?.results.map((group) => (
-        <div key={group.id} className="flex justify-center items-center">
+        <div 
+          key={group.id} 
+          className="flex justify-center items-center cursor-pointer"
+          onClick={() => handleGroupClick(group.id)}
+        >
           <DirectMessagesCard
             name={group.name}
             icon={"/electron.svg"}

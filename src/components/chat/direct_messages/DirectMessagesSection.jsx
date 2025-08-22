77c59ea704eeaf427ee182aa73b7854a2155ed45
fratsaislam <i_fratsa@estin.dz>
2025-08-22 18:59:08 +0100

    fix time of the last message on the card + dynamic last message + dynamic last message sender

--- src/components/chat/direct_messages/DirectMessagesSection.jsx
+++ src/components/chat/direct_messages/DirectMessagesSection.jsx
@@ -1,29 +1,52 @@
 import DirectMessagesCard from "./DirectMessagesCard";
 import useGroupStore from "../../../stores/groupStore";
+import useUserStore from "../../../stores/userStore";
 import { useRouter } from "next/navigation";
+
 export default function DirectMessagesSection() {
   const { groups } = useGroupStore();
-  const router = useRouter()
-
+  const { user } = useUserStore();
+  const router = useRouter();
+  
   const handleGroupClick = (groupId) => {
     router.push(`/chat/groups/${groupId}`);
   };
 
+  const formatLastMessage = (group) => {
+    if (group.last_message_sender === user?.username) {
+      return `You: ${group.last_message}`;
+    } else {
+      return `${group.last_message_sender}: ${group.last_message}`;
+    }
+  };
+
+  const groupsList = groups?.results || groups || [];
+
   return (
     <div className="flex flex-col pt-6">
-      {groups?.results.map((group) => (
-        <div 
-          key={group.id} 
-          className="flex justify-center items-center cursor-pointer"
-          onClick={() => handleGroupClick(group.id)}
-        >
-          <DirectMessagesCard
-            name={group.name}
-            icon={"/electron.svg"}
-            lastMessage={"hey how have you been ?"}
-          />
+      {groupsList.length > 0 ? (
+        groupsList.map((group) => {
+          return (
+            <div
+              key={group.id}
+              className="flex justify-center items-center cursor-pointer"
+              onClick={() => handleGroupClick(group.id)}
+            >
+              <DirectMessagesCard
+                name={group.name}
+                icon={group.icon || "/electron.svg"}
+                lastMessage={formatLastMessage(group)}
+                lastMessageTime={group.last_message_date}
+                unreadCount={group.unreadCount || group.unread_count || 0}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div className="flex justify-center items-center p-4">
+          <span className="text-[var(--lighter-color)]">No groups found</span>
         </div>
-      ))}
+      )}
     </div>
   );
 }

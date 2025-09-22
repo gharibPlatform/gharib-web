84b3a95a7306097ed4428e08d25c99ca1af3e0ed
KMalek101 <kaoucheaneslive@gmail.com>
2025-09-22 14:22:33 +0100

    edit on apiGroup file

--- src/utils/group/apiGroup.js
+++ src/utils/group/apiGroup.js
@@ -7,7 +7,7 @@ export const getGroups = async (groupId) => {
   try {
     const url = groupId ? `/group/?group_id=${groupId}` : `/group/`;
     const res = await api.get(`${API_BASE_URL}${url}`);
-    return res.data.results;
+    return res.data;
   } catch (error) {
     console.error("Error creating group:", error);
   }
